7e50096f6bf416007c6d626aa9525af40ef3724d
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-13 18:41:26 +0300

    feat: add countries api with axios

---
+++ src/lib/countriesAPI.ts
@@ -0,0 +1,23 @@
+import { AxiosError, AxiosResponse } from 'axios'
+
+const axios = require('axios').default
+
+const url = 'https://restcountries.com/v3.1'
+
+interface CountryInterface {
+	name: string
+	capital: string
+	region: string
+	flags: {
+		png: string
+		svg: string
+	}
+}
+
+export const fetchAllCountries = (): CountryInterface[] => {
+	return axios(`${url}/all`)
+		.then((response: AxiosResponse<CountryInterface[]>) => {
+			return response.data
+		})
+		.catch((error: AxiosError) => error)
+}
