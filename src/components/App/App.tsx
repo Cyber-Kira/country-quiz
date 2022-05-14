5bc929e4a8172f52f5bb310537066c0fd0f99566
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-14 18:30:22 +0300

    feat: add react router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,17 @@
-import React, { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { getCountries } from '../../features/countries/countriesSlice'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { NotFound } from '../NotFound/NotFound'
+import { SharedLayout } from '../SharedLayout/SharedLayout'
 
 export const App = () => {
-	const { data } = useAppSelector(store => store.countries)
-	const dispatch = useAppDispatch()
-
-	useEffect(() => {
-		dispatch(getCountries())
-	}, [])
-
-	const items = (
-		<div>
-			<ul>
-				{data.map(item => {
-					return (
-						<li
-							className='block my-10 first:mt-0 last:mb-0'
-							key={item.name.common}
-						>
-							<div>
-								<p>name: {item.name.common}</p>
-								<p>capital: {item.capital}</p>
-								flag: <img src={item.flags.png} alt={item.name.common} />
-							</div>
-						</li>
-					)
-				})}
-			</ul>
-		</div>
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<SharedLayout />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</Router>
 	)
-
-	return <div className='text-xl font-mono'>{items}</div>
 }
 
 export default App
