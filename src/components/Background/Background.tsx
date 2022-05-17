3b1c0af4633575a337af1a41ee0faa701a4ebf26
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-17 18:27:33 +0300

    feat: add background image

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import img from './assets/background.png'
 
 export const Background = () => {
-  return <div>Background</div>
+	return (
+		<img
+			className='invisible md:visible fixed inset-0 object-cover bg-no-repeat h-full w-full -z-10'
+			src={img}
+			alt='Background'
+		/>
+	)
 }
