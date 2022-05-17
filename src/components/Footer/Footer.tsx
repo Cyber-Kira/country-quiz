180b6f7dce59a2e3c5d1a6faf99885914cc6763d
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-17 18:28:05 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+	return (
+		<div className='font-montserrat text-sm text-white-100 leading-4 font-normal md:fixed md:bottom-5 md:left-1/2 md:-translate-x-1/2'>
+			created by
+			<a
+				href='https://devchallenges.io/portfolio/Cyber-Kira'
+				target='_blank'
+				rel='noreferrer'
+				className='font-montserrat text-sm leading-4 font-bold px-1 underline underline-offset-1 hover:text-white-50 focus:text-white-50 transition-colors'
+			>
+				CyberKira
+			</a>
+			- devChallenges.io
+		</div>
+	)
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				montserrat: ['Montserrat', 'sans-serif'],
+				poppins: ['Poppins', 'sans-serif'],
+			},
+		},
+		colors: {
+			transparrent: 'transparent',
+			white: {
+				50: '#FFFFFF',
+				100: '#F2F2F2',
+			},
+			primary: {
+				50: '#6066D0CC',
+				100: '#2F527B',
+				200: '#1D355S',
+			},
+		},
+	},
+	plugins: [],
+}
