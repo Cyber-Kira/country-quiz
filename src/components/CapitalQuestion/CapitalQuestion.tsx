36302097683989ba30d9b6b88a5009a39256267e
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-18 19:14:39 +0300

    update

--- src/components/CapitalQuestion/CapitalQuestion.tsx
+++ src/components/CapitalQuestion/CapitalQuestion.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import img from './assets/undraw_adventure_4hum 1.svg'
+import { AppPaper } from '../AppPaper'
+import { QuestionList } from '../QuestionList'
 
 export const CapitalQuestion = () => {
+	const countries = ['Vietnam', 'Malaysia', 'Sweden', 'Austria']
 	return (
-		<div className='w-full px-6 py-4 md:p-0 max-w-[464px]'>
-			<div className='flex justify-between'>
-				<h1 className='font-poppins font-bold text-2xl leading-[64px] md:text-4xl text-primary-100 md:text-white-100 uppercase md:mt-4'>
-					Country quiz
-				</h1>
-				<img
-					className='w-28 md:w-40'
-					src={img}
-					alt='The guy standing by the globe'
-				/>
-			</div>
-		</div>
+		<AppPaper question='Kuala Lumpur is the capital of'>
+			<QuestionList countries={countries} />
+		</AppPaper>
 	)
 }
