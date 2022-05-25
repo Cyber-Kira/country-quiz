c6ee151c45389dc01c680db34e31c81509949953
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-25 22:42:32 +0300

    feat: remove hard coded question

--- src/components/CapitalQuestion/CapitalQuestion.tsx
+++ src/components/CapitalQuestion/CapitalQuestion.tsx
@@ -3,10 +3,9 @@ import { AppPaper } from '../AppPaper'
 import { QuestionList } from '../QuestionList'
 
 export const CapitalQuestion = () => {
-	const countries = ['Vietnam', 'Malaysia', 'Sweden', 'Austria']
 	return (
-		<AppPaper question='Kuala Lumpur is the capital of'>
-			<QuestionList countries={countries} />
+		<AppPaper>
+			<QuestionList />
 		</AppPaper>
 	)
 }
