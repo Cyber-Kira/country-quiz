77463635718d8027774bc74e5b8f4af0da8c575c
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-06-03 10:03:44 +0300

    feat: add component to select games

---
+++ src/components/GameSelector/GameSelector.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { AppPaper } from '../AppPaper'
+
+export const GameSelector = () => {
+	return (
+		<AppPaper>
+			<>
+				<h2>Select your game type</h2>
+				<div className='grid'>
+					<div className='w-1/2 h-12 flex items-center justify-center rounded-lg text-white-50 font-poppins text-lg uppercase bg-primary-50'>
+						<Link to='/capital'>Country Quiz</Link>
+					</div>
+				</div>
+			</>
+		</AppPaper>
+	)
+}
