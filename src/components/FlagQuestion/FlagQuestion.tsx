32a04fd0bb430acdd2ddc15fa2a4044afda3246f
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-07-11 18:47:34 +0300

    feat: add flag question component

--- src/components/FlagQuestion/FlagQuestion.tsx
+++ src/components/FlagQuestion/FlagQuestion.tsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { getCountries } from '../../features/countries/countriesSlice'
+import { AppPaper } from '../AppPaper'
+import { ErrorBoundary } from '../ErrorBoundary/ErrorBoundary'
+import { QuestionList } from '../QuestionList'
 
 export const FlagQuestion = () => {
-	return <div>Flag Question</div>
+	const { data } = useAppSelector(store => store.countries)
+	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		if (data.length === 0) {
+			dispatch(getCountries())
+		}
+	}, [data])
+
+	return (
+		<AppPaper>
+			<ErrorBoundary>
+				<QuestionList flags />
+			</ErrorBoundary>
+		</AppPaper>
+	)
 }
